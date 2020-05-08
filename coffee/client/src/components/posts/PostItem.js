257f3e584592a792e5a7a0e05d1ac02962a87865
richardhan <hxy.314@gmail.com>
2020-05-08 22:38:54 +1000

    refactor

--- coffee/client/src/components/posts/PostItem.js
+++ coffee/client/src/components/posts/PostItem.js
@@ -20,11 +20,10 @@ const PostItem = ({
   return (
   <div className='post bg-white p-1 my-1'>
     <div>
-      <p>shop name: <Link to={`/shops/${_id}`}> {text}</Link></p>
+      <p>Shop name: <Link to={`/shops/${_id}`}> {text}</Link></p>
       <p className='post-date'>
         created cafe on <Moment format='YYYY/MM/DD'>{date}</Moment>
       </p>
-
       <div><b>average rate:</b> {hasValue(averageReview)}</div>
       <div><b>total review number:</b> {hasValue(totalReview)}</div>
       <div><b>address:</b> {hasValue(address)}</div>
